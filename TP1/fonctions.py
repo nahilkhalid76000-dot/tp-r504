dfd8799ceeec4890d5c3df61db2e8aa9aa3087a3
nahilkhalid76000-dot <nahilkhalid76000@gmail.com>
2026-09-08 16:34:39 +0200

    Réécriture de puissance

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,6 +1,19 @@
 def puissance(a, b):
     if not type(a) is int or not type(b) is int:
         raise TypeError("Only integers are allowed")
+    
     if a == 0 and b < 0:
         raise ValueError("0 elevated to a negative power is undefined")
-    return a ** b
+    
+    if b == 0:
+        return 1
+    
+    res = 1
+    exp = abs(b)
+    for _ in range(exp):
+        res *= a
+        
+    if b < 0:
+        return 1 / res
+        
+    return res
