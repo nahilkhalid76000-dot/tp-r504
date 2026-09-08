09b8264e59ace3609ffef957ec5561b8936be3a7
nahilkhalid76000-dot <nahilkhalid76000@gmail.com>
2026-09-08 16:06:57 +0200

    Gestion de la puissance et tests

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,4 +1,6 @@
 def puissance(a, b):
     if not type(a) is int or not type(b) is int:
         raise TypeError("Only integers are allowed")
+    if a == 0 and b < 0:
+        raise ValueError("0 elevated to a negative power is undefined")
     return a ** b
--- TP1/test.py
+++ TP1/test.py
@@ -10,3 +10,8 @@ def test_2():
     assert f.puissance(-1, 3) == -1
     assert f.puissance(-1, -1) == -1
     assert f.puissance(-2, -1) == -0.5
+
+def test_3():
+    assert f.puissance(0, 5) == 0
+    with pytest.raises(ValueError):
+        f.puissance(0, -1)
